80a30012ae9d8915e251138726c05ef37dae780e
nartco <sebastien.thuillier@hotmail.com>
2020-09-15 22:34:57 +0200

    adding places

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button
+} from "react-native";
+import { useDispatch } from "react-redux";
+import Colors from "../constants/Colors";
+import * as placesAction from '../store/places-actions'
 
 const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState("");
+
+  const dispatch = useDispatch()
+
+  const titleChangeHandler = text => {
+    setTitleValue();
+  };
+
+  const savePlaceHandler = () => {
+    dispatch(placesAction.addPlace(titleValue))
+  };
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <Button
+          title='save place'
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+NewPlaceScreen.navigationOptions = {
+  headerTitle: "Add Place"
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
 
 export default NewPlaceScreen;
