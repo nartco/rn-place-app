9c37aa9446532774b4bfbd62f54f92a32f49bee4
nartco <sebastien.thuillier@hotmail.com>
2020-09-16 00:10:26 +0200

    access to camera

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -10,6 +10,7 @@ import {
 import { useDispatch } from "react-redux";
 import Colors from "../constants/Colors";
 import * as placesAction from '../store/places-actions'
+import ImagePicker from '../components/ImagePicker'
 
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState("");
@@ -33,6 +34,7 @@ const NewPlaceScreen = props => {
           onChangeText={titleChangeHandler}
           value={titleValue}
         />
+        <ImagePicker />
         <Button
           title='save place'
           color={Colors.primary}
