72ddd07a0a5387d65e42fc70d6779a756723a25f
nartco <sebastien.thuillier@hotmail.com>
2020-09-18 23:20:13 +0200

    finish

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,12 +1,27 @@
 import * as FileSystem from "expo-file-system";
 
 import { insertPlace, fetchPlaces } from "../helpers/db";
+import ENV from "./env";
 
 export const ADD_PLACE = "ADD_PLACE";
 export const SET_PLACES = "SET_PLACES";
 
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
   return async dispatch => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${ENV.googleApiKey}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+    if (!resData.results) {
+      throw new Error("Something went wrong!");
+    }
+    const address = resData.results[0].formatted_address
+
     const fileName = image.split("/").pop();
     //   someFolder/myImage.jpg => ['someFolder', 'myImage.jpg'] => myImage.jpg
     const newPath = FileSystem.documentDirectory + fileName;
@@ -19,14 +34,17 @@ export const addPlace = (title, image) => {
       const dbResult = await insertPlace(
         title,
         newPath,
-        "Dummy address",
-        15.6,
-        12.3
+        address,
+        location.lat,
+        location.lng
       );
       console.log(dbResult);
       dispatch({
         type: ADD_PLACE,
-        placeData: { id: dbResult.insertId, title: title, image: newPath }
+        placeData: { id: dbResult.insertId, title: title, image: newPath, address: address, coords: {
+          lat: location.lat,
+          lng: location.lng
+        } }
       });
     } catch (err) {
       console.log(err);
@@ -39,10 +57,10 @@ export const loadPlaces = () => {
   return async dispatch => {
     try {
       const dbResult = await fetchPlaces();
-      console.log(dbResult)
+      console.log(dbResult);
       dispatch({ type: SET_PLACES, places: dbResult.rows._array });
     } catch (err) {
-        throw err
+      throw err;
     }
   };
 };
